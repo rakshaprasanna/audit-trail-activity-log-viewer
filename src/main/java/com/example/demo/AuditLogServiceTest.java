43c80d742846febec8805ebe686ac515b993da03
Punyashree <punyashree26@gmail.com>
2026-05-08 18:45:50 +0530

    Day 8 — Added global exception handler and basic tests

---
+++ src/main/java/com/example/demo/AuditLogServiceTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+public class AuditLogServiceTest {
+
+    @Test
+    void testService() {
+
+        System.out.println("Service Test Passed");
+    }
+}
---
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+
+        return new ResponseEntity<>(
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+}
